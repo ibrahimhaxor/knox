9a444f548604cd5e0aec0fb48e0a422dfc8003c4
Ibrahim Abduurrahman Net <103115605+ibrahimhaxor@users.noreply.github.com>
2026-08-10 15:16:43 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ if (
     return res.status(200).json({
         success: true,
         message: "OK",
-        operation: "check",
+        operation: "spd",
         credits: 250,
         balance: 250,
         credits_left: 250,
